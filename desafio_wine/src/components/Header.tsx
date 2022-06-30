98de0ed56b9d9256626636e6e11cbe58c4d2a307
fernandosenacruz <fernandosenacruz@gmail.com>
2022-06-29 22:01:26 -0300

    Update blacklogo_wine.svg and Header.tsx

--- desafio_wine/src/components/Header.tsx
+++ desafio_wine/src/components/Header.tsx
@@ -1,9 +1,28 @@
-import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return (
         <header >
-            <span>Wine</span>
+            <div>
+                <Image
+                    src='/blacklogo_wine.svg'
+                    alt="country flag"
+                    width="100px"
+                    height="28px"
+                />
+            </div>
+            <div>
+                <h6>Clube</h6>
+                <h6>Loja</h6>
+                <h6>Produtores</h6>
+                <h6>Ofertas</h6>
+                <h6>Eventos</h6>
+            </div>
+            <div>
+                <span>lupa</span>
+                <span>user</span>
+                <span>carrinho</span>
+            </div>
         </header>
     )
 }
