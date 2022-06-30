602a3e23ea5f9c4013f78c70dff8eca838cba728
fernandosenacruz <fernandosenacruz@gmail.com>
2022-06-29 22:01:39 -0300

    Update index.tsx and index.tsx

--- desafio_wine/src/pages/index.tsx
+++ desafio_wine/src/pages/index.tsx
@@ -2,9 +2,9 @@ import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 import ProductList from '../components/ProductList'
-import { IProduct } from '../components/Product'
+import { Http } from '../http/api'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="main">
-        <ProductList products={products}/>
+        <ProductList products={props.products.items}/>
       </main>
       
     </div>
@@ -22,48 +22,12 @@ const Home: NextPage = () => {
 export default Home
 
 export const getStaticProps: GetStaticProps = async (context) => {
+  const { data } = await Http.get(
+                'products?page=1&limit=10'
+            )
     return {
         props: {
-            products
+            products: data
         }
     }
 }
-
-export const products: IProduct[] = [
-    {
-      avaliations: 4,
-      rating: 4,
-      classification: "Seco",
-      country: "Chile",
-      discount: 31,
-      flag: "https://img.wine.com.br/fenix/image/flags/rounded/chile.svg",
-      id: 3,
-      image: "https://www.wine.com.br/cdn-cgi/image/f=png,h=515,q=99/assets-images/produtos/19252-01.png",
-      name: "Canepa Novísimo Cabernet Sauvignon 2017",
-      price: 241.9,
-      priceMember: 166.55,
-      priceNonMember: 195.94,
-      region: "Valle Central",
-      sommelierComment: "Jovem e com um estilo que agrada com facilidade, esse versátil rótulo pode ser degustado sozinho ou harmonizado com receitas do dia a dia. A Viña Canepa, fundada no Chile por imigrantes italianos, iniciou a produção da linha Novísimo nos anos 80.",
-      type: "Tinto",
-      size: "750 ml",
-    },
-    {
-      avaliations: 292,
-      classification: "Seco",
-      country: "Portugal",
-      discount: 45,
-      flag: "https://img.wine.com.br/fenix/image/flags/rounded/portugal.svg",
-      id: 0,
-      image: "https://www.wine.com.br/cdn-cgi/image/f=png,h=515,q=99/assets-images/produtos/19694-01.png",
-      name: "Bacalhôa Quinta da Garrida Colheita Selecionada 2015",
-      price: 175.9,
-      priceMember: 96.9,
-      priceNonMember: 114,
-      rating: 4,
-      region: "Dão",
-      size: "750 ml",
-      sommelierComment: "Esse tinto traz em seu assemblage a Touriga Nacional, uma das uvas tintas portuguesas mais conhecidas no mundo do vinho. Nativa do Dão, região produtora desse vinho, essa variedade atinge a sua expressão máxima nesse terroir. O termo Colheita Selecionada estampado no rótulo, é uma referência a qualidade da safra 2015.",
-      type: "Tinto",
-    }
-]
