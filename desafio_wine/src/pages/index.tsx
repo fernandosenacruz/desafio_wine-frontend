c0587491926fa6a595a1e551b803c4b7d16aef6c
fernandosenacruz <fernandosenacruz@gmail.com>
2022-07-01 14:34:07 -0300

    Update index.tsx

--- desafio_wine/src/pages/index.tsx
+++ desafio_wine/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
 import styles from '../../styles/Home.module.css'
 import ProductList from '../components/ProductList'
-import { Http } from '../http/api'
 
 const Home: NextPage = (props) => {
   return (
