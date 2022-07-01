a7183fcc6878470d98af75e0703919337588ac71
fernandosenacruz <fernandosenacruz@gmail.com>
2022-06-30 21:25:10 -0300

    Update context.tsx

---
+++ desafio_wine/src/context/context.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { IProduct, IProductProps } from "../components/Product"
+
+type ContextType = {
+  products: IProduct[];
+  setProducts: React.Dispatch<React.SetStateAction<never[]>>;
+};
+
+export const ProductsContext = React.createContext<ContextType>({
+  products: [],
+  setProducts: () => void 0,
+});
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ProductsProvider: React.FC<Props> = ({ children }) => {
+  const [products, setProducts] = React.useState([]);
+
+  return (
+    <ProductsContext.Provider value={{ products, setProducts }}>
+      {children}
+    </ProductsContext.Provider>
+  );
+};
+
+export default ProductsProvider;
