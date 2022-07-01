2cb1f4e307bce382bc0e6e2e2fcb4458f10c026f
fernandosenacruz <fernandosenacruz@gmail.com>
2022-07-01 14:34:01 -0300

    Update context.tsx

--- desafio_wine/src/context/context.tsx
+++ desafio_wine/src/context/context.tsx
@@ -1,14 +1,18 @@
 import React from "react"
-import { IProduct, IProductProps } from "../components/Product"
+import { IProduct } from "../components/Product"
 
 type ContextType = {
   products: IProduct[];
   setProducts: React.Dispatch<React.SetStateAction<never[]>>;
+  product: IProduct;
+  setProduct: React.Dispatch<React.SetStateAction<never[]>>;
 };
 
 export const ProductsContext = React.createContext<ContextType>({
   products: [],
   setProducts: () => void 0,
+  product: {},
+  setProduct: () => void 0,
 });
 
 type Props = {
@@ -17,9 +21,10 @@ type Props = {
 
 const ProductsProvider: React.FC<Props> = ({ children }) => {
   const [products, setProducts] = React.useState([]);
+  const [product, setProduct] = React.useState({});
 
   return (
-    <ProductsContext.Provider value={{ products, setProducts }}>
+    <ProductsContext.Provider value={{ products, setProducts, product, setProduct }}>
       {children}
     </ProductsContext.Provider>
   );
